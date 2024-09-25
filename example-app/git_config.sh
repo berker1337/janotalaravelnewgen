f7f04f7e36081d090d67c4e1d758d3b3394caeea
Vojtěch Janota <vojta.janota10@gmail.com>
2024-09-25 07:46:19 +0200

    fix: :art: pohoda
    .
    
    .

---
+++ example-app/git_config.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "Welcome to the git config"
+git config --global user.name "Vojtěch Janota"
+git config --global user.email "vojta.janota10@gmail.com"
+# git clone
+git config --list
